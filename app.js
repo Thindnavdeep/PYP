d5fefaa642073f7a93098e5a5378ac400fe7e360
Thindnavdeep <navdeepthind@gmail.com>
2023-10-20 00:23:09 +0530

    four

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require('./models/db');
+const cors = require("cors");
 const session = require('express-session');
 
 var indexRouter = require('./routes/index');
@@ -21,8 +22,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-// app.set('views', __dirname + '/views');
-
+app.set('views', __dirname + '/views');
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
